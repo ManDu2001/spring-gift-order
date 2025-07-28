4d0747c86089e79f0aaeb2a8737c1aed6e479c20
WonJuN Sun <swj010324@naver.com>
2025-07-28 14:17:32 +0900

    refactor(gift) : refactor to use ConfigurationPropertiesScan annotation

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -3,10 +3,11 @@ package gift;
 import gift.config.KakaoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(KakaoProperties.class)
+@ConfigurationPropertiesScan
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
