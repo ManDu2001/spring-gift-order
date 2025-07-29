54689cb4f3ea7a3391294c65e3b955b4865bee6d
WonJuN Sun <swj010324@naver.com>
2025-07-29 15:12:05 +0900

    feat(domain) : add Order entity containing information about order

---
+++ src/main/java/gift/domain/Order.java
@@ -0,0 +1,70 @@
+package gift.domain;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private Long optionId;
+
+  private Integer quantity;
+
+  private String message;
+
+  private LocalDateTime orderDateTime;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id", nullable = false)
+  private Member member;
+
+  protected Order() {}
+
+  public static Order of(Member member, Long optionId, Integer quantity, String message) {
+    Order order = new Order();
+    order.assignMember(member);
+    order.optionId = optionId;
+    order.quantity = quantity;
+    order.message = message;
+    return order;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public Long getOptionId() {
+    return optionId;
+  }
+
+  public Integer getQuantity() {
+    return quantity;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public LocalDateTime getOrderDateTime() {
+    return orderDateTime;
+  }
+
+  public Member getMember() {
+    return member;
+  }
+
+  @PrePersist
+  public void prePersist() {
+    this.orderDateTime = LocalDateTime.now();
+  }
+
+  public void assignMember(Member member) {
+    this.member = member;
+    member.getOrders().add(this);
+  }
+}
