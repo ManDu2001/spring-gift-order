ee6af182b48a2f5cc926d243aec32d72e9904d9e
WonJuN Sun <swj010324@naver.com>
2025-07-29 15:26:01 +0900

    feat(service): add method to receive user information with access token, and add method to proceed member register and login when access with auth code

--- src/main/java/gift/service/KakaoOAuthService.java
+++ src/main/java/gift/service/KakaoOAuthService.java
@@ -1,7 +1,16 @@
 package gift.service;
 
+import gift.domain.Member;
 import gift.dto.KakaoOAuthResponseDto;
+import gift.dto.KakaoUserInfoResponseDto;
+import gift.dto.LoginResponseDto;
 
 public interface KakaoOAuthService {
   KakaoOAuthResponseDto getAccessToken(String authorizationCode);
+
+  KakaoUserInfoResponseDto getUserInfo(String accessToken);
+
+  LoginResponseDto registerOrLogin(String authorizationCode);
+
+  Member findMemberByAccessToken(String accessToken);
 }
